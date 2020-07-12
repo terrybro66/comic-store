6adeadf3f0069f73b79cb86454ca15301236bdea
sambroughton <sjbroughton1@gmail.com>
2020-07-11 20:56:10 -0500

    Api token authentication setup

--- api/comics/views.py
+++ api/comics/views.py
@@ -3,10 +3,12 @@ from rest_framework import viewsets
 from .models import Comic
 from .serializers import ComicSerializer
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
+
 
 class ComicView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     search_fields = ['name']
     filter_backends = (filters.SearchFilter,)
     queryset = Comic.objects.all()
     serializer_class = ComicSerializer
-
