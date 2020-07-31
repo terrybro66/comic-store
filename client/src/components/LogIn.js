7b1de556cea6a72475def26ccb4cab07dd9f26bb
terrybro66 <terrybro61@gmail.com>
2020-07-31 17:44:03 +0100

    added config

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -4,45 +4,61 @@ import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import fetcher from "../utils/api/fetcher";
 import styles from "./LogIn.module.scss";
+import formData from "./formData";
 
 const LogIn = () => {
   const history = useHistory();
   const { saveUser } = useAuth();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+
+  const { data, fields } = formData;
+  console.log("fields", fields);
+
+  const [datum, setData] = useState(
+    fields.map((field) => ({
+      ...field,
+      name: field.name,
+      value: "",
+    }))
+  );
+
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
 
   const handleLogIn = async (e) => {
     e.preventDefault();
+    console.log(datum);
     const { data } = await fetcher.post("login/", {
-      username,
-      password,
+      // datum,
+      // password,
     });
     saveUser(data.access);
     history.push("/");
+  };
+  const handleChange = (e) => {
+    console.log(e.currentTarget.value);
+  };
+
+  const Field = ({ field, onChange }) => {
+    const { label, ...attributes } = field;
+
+    return (
+      <>
+        <label>{label}</label>
+        <input onChange={onChange} {...attributes} />
+      </>
+    );
   };
 
   return (
     <div className={styles.darkTheme}>
       <form onSubmit={handleLogIn}>
-        <h1>Log in to your account</h1>
-        <label>Email address</label>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email address"
-          value={username}
-          onChange={(e) => setUsername(e.currentTarget.value)}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
+        <h1>{data.title}</h1>
 
-        <input type="submit" value="Log in" />
+        {fields.map((field) => (
+          <Field key={field.label} field={field} onChange={handleChange} />
+        ))}
+
+        <input type="submit" value={data.buttonText} />
         <div className={styles.signup}>
           Not registered? <Link to={"/sign-up"}>Sign up here</Link>
         </div>
---
+++ client/src/components/formData.js
@@ -0,0 +1,20 @@
+const formData = {
+  data: {
+    title: "Log into your account",
+    buttonValue: "Log in",
+  },
+  fields: [
+    {
+      label: "Email Address",
+      type: "text",
+      name: "username",
+    },
+    {
+      label: "Password",
+      type: "password",
+      name: "password",
+    },
+  ],
+};
+
+export default formData;
